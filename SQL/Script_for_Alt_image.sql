bce5449bd320003a0db68ba6f27d321007fd796a
Jokertwo <p.lastovka@seznam.cz>
2017-05-13 16:24:28 +0200

    script pro Wordpress databazi

--- SQL/Script_for_Alt_image.sql
+++ SQL/Script_for_Alt_image.sql
@@ -1,10 +1,10 @@
 INSERT INTO wp_postmeta 
 	(post_id,meta_key,meta_value)
-    select ID,
+    SELECT ID,
 			'_wp_attachment_image_alt',
 			post_title
 		FROM wp_posts
-			WHERE post_mime_type LIKE 'image/%' and
-				ID not in (select post_id 
-									from wp_postmeta
-                                    where meta_key = '_wp_attachment_image_alt')
+			WHERE post_mime_type LIKE 'image/%' AND
+				ID NOT IN (SELECT post_id 
+									FROM wp_postmeta
+                                    WHERE meta_key = '_wp_attachment_image_alt')

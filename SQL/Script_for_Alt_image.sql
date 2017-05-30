feace81121c47f06b178acbd2b9219e4dd018c4c
Jokertwo <p.lastovka@seznam.cz>
2017-05-30 22:54:26 +0200

    Script pro update databaze
    Přidání komentářu

--- SQL/Script_for_Alt_image.sql
+++ SQL/Script_for_Alt_image.sql
@@ -1,9 +1,10 @@
-INSERT INTO wp_postmeta 
-	(post_id,meta_key,meta_value)
-    SELECT ID,
-			'_wp_attachment_image_alt',
-			post_title
-		FROM wp_posts
+INSERT INTO wp_postmeta -- vloz radek do tabulky wp_postmeta
+	(post_id,meta_key,meta_value) -- vycet do kterych sloupcu se bude vkladat
+    -- vnoreny select z dalsi tabuky, 
+    SELECT ID, -- hodnota ID se vlozi do post_id
+			'_wp_attachment_image_alt', -- vlozi se do sloupce meta_key 
+			post_title -- vlozi se do sloupce meta_value
+		FROM wp_posts -- z ktere tabulky vládam
 			WHERE post_mime_type LIKE 'image/%' AND
 				ID NOT IN (SELECT post_id 
 									FROM wp_postmeta

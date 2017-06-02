b65531add44be92f4d791633ef78bba3a245dad9
Jokertwo <p.lastovka@seznam.cz>
2017-06-02 12:30:28 +0200

    Sidebar
    Odstranění center

--- PHP/sidebar.php
+++ PHP/sidebar.php
@@ -1,5 +1,5 @@
 <div class='social'>
-	<center>
+	
 		<a href='https://www.facebook.com/czechamericantv' class='ico facebook' target='_blank'></a>
 		<a href='https://twitter.com/czechamericantv' class='ico twitter' target='_blank'></a>
 		<a href='https://plus.google.com/u/0/+CatvusaCzechAmericanTV/posts' class='ico gplus' target='_blank'></a>
@@ -16,5 +16,5 @@
 			</a>
 		</p>
 		<iframe src="//www.facebook.com/plugins/likebox.php?adapt_container_width=true&app_id=113869198637480&container_width=588&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fczechamericantv&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=230" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:258px;" allowTransparency="true" class='fbLikeBox'></iframe>
-	</center>
+	
 </div>

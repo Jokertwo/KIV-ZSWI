e925e24d3d61db6e9fbcca277b8c573651f68687
Jokertwo <p.lastovka@seznam.cz>
2017-06-02 13:07:00 +0200

    Siderbar
    Uprava pro standart HTML5

--- PHP/sidebar.php
+++ PHP/sidebar.php
@@ -15,6 +15,6 @@
 				<img src="<?=get_template_directory_uri();?>/images/czechtourism.jpg" alt="">
 			</a>
 		</p>
-		<iframe src="//www.facebook.com/plugins/likebox.php?adapt_container_width=true&app_id=113869198637480&container_width=588&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fczechamericantv&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=230" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:258px;" allowTransparency="true" class='fbLikeBox'></iframe>
+		<iframe src="//www.facebook.com/plugins/likebox.php?adapt_container_width=true&app_id=113869198637480&container_width=588&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fczechamericantv&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=230" style="border:none; overflow:hidden; height:258px;"  class='fbLikeBox'></iframe>
 	
 </div>

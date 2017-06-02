66acd8cc49e672a3280c04cd5e3ec6a8c9c5836f
Jokertwo <p.lastovka@seznam.cz>
2017-06-02 12:41:53 +0200

    Paticka
    Původní soubor

---
+++ PHP/footer.php
@@ -0,0 +1,11 @@
+		<?php include('parts/partners.php'); ?>
+	</div>
+	<footer class='footer'>
+		<div class='row'>
+			<?php dynamic_sidebar('footer'); ?>
+		</div>
+		<p class='copyright'>Copyright 2014 Czech-American TV. All rights reserved by Czech-American TV.</p>
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>

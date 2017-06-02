fedc4e1987c1a487fd9aec82e8defd9f3a7fd971
Jokertwo <p.lastovka@seznam.cz>
2017-06-02 13:34:25 +0200

    SideBar facebook
    Uprava tak aby vyhovoval jak html5 tak i IE prohlizecum

--- PHP/sidebar.php
+++ PHP/sidebar.php
@@ -1,4 +1,4 @@
-<div class='social'>
+<div class='social' style="text-align: center;">
 	
 		<a href='https://www.facebook.com/czechamericantv' class='ico facebook' target='_blank'></a>
 		<a href='https://twitter.com/czechamericantv' class='ico twitter' target='_blank'></a>
@@ -15,6 +15,13 @@
 				<img src="<?=get_template_directory_uri();?>/images/czechtourism.jpg" alt="">
 			</a>
 		</p>
-		<iframe src="//www.facebook.com/plugins/likebox.php?adapt_container_width=true&app_id=113869198637480&container_width=588&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fczechamericantv&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=230" style="border:none; overflow:hidden; height:258px;"  class='fbLikeBox'></iframe>
+		
+	
+	<!--[if IE]>
+			<iframe src="//www.facebook.com/plugins/likebox.php?adapt_container_width=true&app_id=113869198637480&container_width=588&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fczechamericantv&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=230" scrolling="no" frameborder="0" style="height:258px;" allowTransparency="true" class='fbLikeBox'></iframe>
+		<![endif]-->
+	<!--[if !IE]><!-->
+			<iframe src="//www.facebook.com/plugins/likebox.php?adapt_container_width=true&app_id=113869198637480&container_width=588&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fczechamericantv&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=230" style="border:none; overflow:hidden; height:258px;"  class='fbLikeBox'></iframe>
+		<!--<![endif]-->
 	
 </div>

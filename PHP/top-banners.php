cc0b13503431c8cabed415e821715d898c2ee9b3
Jokertwo <p.lastovka@seznam.cz>
2017-06-03 12:12:23 +0200

    Top banner
    Doplění alternativního popisu obrázku

--- PHP/top-banners.php
+++ PHP/top-banners.php
@@ -4,7 +4,8 @@ $loop = 0;
 if ( $the_query->have_posts() ) {
     while ( $the_query->have_posts() ) {
         $the_query->the_post();
-        $image = get_field('image');?>
+        $image = get_field('image');
+		?>
         <a href='<?=get_field('url');?>' target='_blank' <? if($loop > 0){?>style='display: none'<?php } ?>>
             <img
                 <?php if ($loop == 0) { ?>src='<?=$image['url'];?>' <?php } 
@@ -15,6 +16,7 @@ if ( $the_query->have_posts() ) {
 						<? }?>
 						
                     data-src='<?=$image['url'];?>'
+					alt='<?=$image['alt'];?>'
                 />
         </a>
         <?php
